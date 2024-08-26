2ef907336a44da046a6079f9718c34fc97473b3e
Duc Nguyen <nguyenjack0406@gmail.com>
2024-08-26 16:59:52 +0700

    August 26, learning about data structure

---
+++ august26_2024.py
@@ -0,0 +1,12 @@
+# Set
+
+game_set = set()
+game_set2 = {"valorant", "roblox"}
+
+print(game_set2)
+
+# cant have any duplicate value
+# no particular order
+
+# Tuples - immutable/unchangable list
+game_tuples = ("valorant", "lq")
