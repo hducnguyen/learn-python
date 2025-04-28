b51e5d533708a98fc40164d96a3557961e149c41
hducnguyen <jackyboy.vinschool@gmail.com>
2025-04-28 13:10:42 +0700

    changes

--- nov13_tictactoe.py
+++ nov13_tictactoe.py
@@ -1,4 +1,4 @@
-# Initialise board dimension
+# Initialize board dimension
 board_game = [[], [], []]
 
 # Fill the board with numbers as moves for the player
@@ -9,50 +9,49 @@ for i in range(3):
         counter += 1
 
 # Display the board
-def display(board_game):
-    for i in range(len(board_game)):
-        for j in range(len(board_game[i])):
-            print(board_game[i][j], end=" ")
+def display(board):
+    for row in board:
+        for cell in row:
+            print(cell, end=" ")
         print()
 
-# Update the moves for the players
-def moveUpdate(playerMoves):
-    indexNumber = int(input("Enter ur move: "))
-    for i in range(len(board_game)):
-        for j in range(len(board_game[i])):
-            if board_game[i][j] == indexNumber:
-                board_game[i][j] = playerMoves
+# Custom procedure with parameters to update the board
+def move_update(board, player_symbol) -> bool:
+    move_number = int(input("Enter your move (1-9): "))
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == move_number:
+                board[i][j] = player_symbol
+                return True  # Return True when a move is successfully made
+    print("Invalid move. Try again.")
+    return False  # Return False if move is invalid
 
-def checkForWin(board_game):
-    if board_game[0][0] == board_game[0][1] == board_game[0][2]:
+# Check if the current board has a winner
+def check_for_win(board):
+    # Check rows, columns, and diagonals
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2]:
+            return True
+        elif board[0][i] == board[1][i] == board[2][i]:
+            return True
+    if board[0][0] == board[1][1] == board[2][2]:
         return True
-    elif board_game[1][0] == board_game[1][1] == board_game[1][2]:
+    if board[0][2] == board[1][1] == board[2][0]:
         return True
-    elif board_game[2][0] == board_game[2][1] == board_game[2][2]:
-        return True
-    elif board_game[0][0] == board_game[1][0] == board_game[2][0]:
-        return True
-    elif board_game[0][1] == board_game[1][1] == board_game[2][1]:
-        return True
-    elif board_game[0][2] == board_game[1][2] == board_game[2][2]:
-        return True
-    elif board_game[0][0] == board_game[1][1] == board_game[2][2]:
-        return True
-    elif board_game[0][2] == board_game[1][1] == board_game[2][0]:
-        return True
-        
-move = True
+    return False
 
+# Main game loop
+current_player = "X"
 while True:
-    if move == True:
-        playerMoves = "x"
-        move = False
-    else:
-        playerMoves = "o"
-        move = True
     display(board_game)
-    moveUpdate(playerMoves)
-    if checkForWin(board_game) == True:
-        print("Winner is " + playerMoves)
-        display(board_game)
-        break
+    successful_move = move_update(board_game, current_player)
+    if successful_move:
+        if check_for_win(board_game):
+            print("Winner is " + current_player)
+            display(board_game)
+            break
+        # Switch player
+        if current_player == "X":
+            current_player = "O"
+        else:
+            current_player = "X"
